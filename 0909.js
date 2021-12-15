7f9ed17e1ff0cd0b505f84e3d641b32951310fda
YYK <1497196254@qq.com>
2021-12-15 18:10:43 +0800

    12.15

--- 0909.js
+++ 0909.js
@@ -1,5 +1,18 @@
-var a=4 ,b=5,c,d=7,e=8,f,x=2,y=3;
-ax + by == c;
-dx + ey == f;
-console.log(c);
-console.log(f);
+var a = parseInt(prompt("a"));
+var b = parseInt(prompt("b"));
+var c = parseInt(prompt("c"));
+
+var delta = b * b - 4 * a * c;
+if (delta < 0) {
+  var sqrtDelta = Math.sqrt(delta);
+} else {
+  if (delta == 0) {
+      var x = (-b) / (2*a)
+      console.log('x1=x2=' + x)
+  } else {
+    var x1 = (-b + sqrtDelta) / (2 * a);
+    var x2 = (-b - sqrtDelta) / (2 * a);
+    alert("x1=" + x1);
+    alert("x2=" + x2);
+  }
+}
