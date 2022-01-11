f028e0e52c9ad2a6d801d177ea8140d0c4b58e5a
YYK <1497196254@qq.com>
2022-01-11 21:22:00 +0800

    lodash

--- lodash/yangyukang9-lodash.js
+++ lodash/yangyukang9-lodash.js
@@ -53,4 +53,43 @@ var yangyukang9 = {
     }
     return res;
   },
+
+  fill: function (array, value, start = 0, end = array.length) {
+    for (let i = start; i < end; i++) {
+      array[i] = value;
+    }
+    return array;
+  },
+
+  head: function (array) {
+    if (!array) return undefined;
+    return array[0];
+  },
+
+  indexOf: function (array, value, fromIndex = 0) {
+    let len = array.length;
+    for (let i = fromIndex; i < len; i++) {
+      if (array[i] == value) return i;
+    }
+    return -1;
+  },
+
+  initial: function (array) {
+    let res = [];
+    for (let i = 0; i < array.length - 1; i++) {
+      res.push(array[i]);
+    }
+    return res;
+  },
+
+  last: function (array) {
+    return array[array.length - 1];
+  },
+
+  lastIndexOf: function (array, value, fromIndex = array.length - 1) {
+    for (let i = fromIndex; i >= 0; i--) {
+      if (array[i] === value) return i;
+    }
+    return -1;
+  },
 };
