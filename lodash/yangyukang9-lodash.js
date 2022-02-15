0db2554843a912d15b363ee1d00314e58eb467ae
YYK <1497196254@qq.com>
2022-02-15 21:56:18 +0800

    lodash

--- lodash/yangyukang9-lodash.js
+++ lodash/yangyukang9-lodash.js
@@ -1,3 +1,5 @@
+const { first, template } = require("lodash");
+
 var yangyukang9 = {
   chunk: function (array, size) {
     let chunks1 = [];
@@ -28,7 +30,24 @@ var yangyukang9 = {
     return compacts;
   },
 
-  concat: function (array, values) {},
+  difference: function (array, ...values) {
+    var ary = [];
+    for (var i = 0; i < array.length; i++) {
+      var item = array[i];
+      var ins = false;
+      for (var j = 0; j < values.length; j++) {
+        for (var k = 0; k < values[j].length; k++) {
+          if (item == values[j][k]) {
+            ins = true;
+          }
+        }
+      }
+      if (!ins) {
+        ary.push(item);
+      }
+    }
+    return ary;
+  },
 
   drop: function (array, n = 1) {
     let res = [];
@@ -91,5 +110,60 @@ var yangyukang9 = {
       if (array[i] === value) return i;
     }
     return -1;
+  },
+
+  keyBy: function (array, propName) {
+    return array.reduce((obj, item) => {
+      obj[item[propName]] = item;
+      return obj;
+    });
+  },
+
+  flatten: function (array) {
+    return [].concat(...array);
+  },
+
+  flattenDeep: function (array) {
+    if (array.length == 0) return [];
+    var result = [];
+    array.forEach((item) => {
+      if (Array.isArray(item)) {
+        result.push(...this.flattenDeep(item));
+      } else {
+        result.push(item);
+      }
+    });
+    return result;
+  },
+
+  flattenDepth: function (array, depth = 1) {
+    if (depth == 0) return array;
+    if (depth == 1) return this.flatten(array);
+
+    var result = this.flatten(array);
+    for (var i = 1; i < depth; i++) {
+      result = this.flatten(result);
+    }
+    return result;
+  },
+
+  fromPairs: function (pairs) {
+    var obj = {};
+    pairs.forEach((item) => {
+      obj[item[0]] = item[1];
+    });
+    return obj;
+  },
+
+  intersection: function (...arrays) {
+    var ary = arrays[0];
+    arrays.forEach((item) => {
+      item.forEach((res, idx) => {
+        if (!ary.includes(res)) {
+          ary.splice(idx, 1);
+        }
+      });
+    });
+    return ary;
   },
 };
